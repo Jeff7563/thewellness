e5a54b29dacf74ae47eba9253f9753ab662d1182
palmmyki <nattidachaisangrat.182334@gmail.com>
2025-01-26 14:57:29 +0700

    ตัดรูปออก

--- php/album1.php
+++ php/album1.php
@@ -82,16 +82,14 @@ if (!isset($_SESSION['user_id'])) {
     <img src="https://image.bangkokbiznews.com/uploads/images/md/2022/05/ObS7NpK4gBKO1dPFs1p7.webp" alt="Photo 2">
     <img src="https://thesmartlocal.ph/wp-content/uploads/2022/11/All-aBoard-XP-BGC-Takenoko-scaled.jpg" alt="Photo 3">
     <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjUo3mzju8AAAVrLN_Y87G-kFavolt2y7i2zdPrc-z8QfKc1JC2YWE7hXsopzpnHXa_L8E97INJpTKFRrIDZjoXG2vuER1nCfWcCxkGI0aRw5CXHb4R0zJJLR5Fdk47vD4dEecytAF-c5WT/s1600/IMG_20180428_154414.jpg" alt="Photo 4">
-    <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjZhpmTzHFdmFQRxoCxePAOcgBb4pWTeTBQ5wlESSLzZPzIY_uDtWDd9NLV2x3XwglTBbV6QA7B5KPyg3ke64KkGcekEbmcpJU5nz9xi-HyPGxFhLSfq3TFqpu77PWGu8PbyWdEY1iwRah7/s1600/IMG_20180428_145634.jpg" alt="Photo 5">
-    <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiejWXuWJOJY1o0vLngwpPgzsPkaKm0Oq4U1XVyuRS6HOaiYTBdM0F8Uck-qmOTIu_DUyFq5fspFreXlb9-gZKXaKuMpkqLlyeDh_T_7utHxOi8rkjdrFtt6cU4hdQmvY4wPimhxJ9P2WOp/s1600/IMG_20180607_113602.jpg" alt="Photo 6">
+   
   </div>
    <div class="photo-grid">
     <img src="https://i.redd.it/ipd324n66cl61.jpg" alt="Photo 7">
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Deskohran%C3%AD_08-09-27_212.jpg/640px-Deskohran%C3%AD_08-09-27_212.jpg" alt="Photo 8">
     <img src="https://www.uplay.it/bggimages/14996/1146379.jpg" alt="Photo 9" >
     <img src="https://www.uplay.it/bggimages/14996/1220301.jpg" alt="Photo 10">
-    <img src="https://www.uplay.it/bggimages/14996/1216204.jpg" alt="Photo 11">
-    <img src="https://www.uplay.it/bggimages/14996/1228301.jpg" alt="Photo 12">
+    
   </div>
 </body>
 </html>
