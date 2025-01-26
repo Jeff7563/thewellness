c8ca8c64edee3de00088b815d5b1579f83757b28
palmmyki <nattidachaisangrat.182334@gmail.com>
2025-01-26 14:48:37 +0700

    เพิ่มรูปทั้งหมดเป็นลิ้งค์ของหน้าalbum1 php

--- php/album1.php
+++ php/album1.php
@@ -81,17 +81,17 @@ if (!isset($_SESSION['user_id'])) {
     <img src="https://th.bing.com/th/id/OIP.c047FeZcAwR3ptUeYSAynAHaE8?rs=1&pid=ImgDetMain alt="Photo 1">
     <img src="https://image.bangkokbiznews.com/uploads/images/md/2022/05/ObS7NpK4gBKO1dPFs1p7.webp" alt="Photo 2">
     <img src="https://thesmartlocal.ph/wp-content/uploads/2022/11/All-aBoard-XP-BGC-Takenoko-scaled.jpg" alt="Photo 3">
-    <img src="" alt="Photo 4">
-    <img src="" alt="Photo 5">
-    <img src="/images/บอร์ดเกม5.jpg" alt="Photo 6">
+    <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjUo3mzju8AAAVrLN_Y87G-kFavolt2y7i2zdPrc-z8QfKc1JC2YWE7hXsopzpnHXa_L8E97INJpTKFRrIDZjoXG2vuER1nCfWcCxkGI0aRw5CXHb4R0zJJLR5Fdk47vD4dEecytAF-c5WT/s1600/IMG_20180428_154414.jpg" alt="Photo 4">
+    <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjZhpmTzHFdmFQRxoCxePAOcgBb4pWTeTBQ5wlESSLzZPzIY_uDtWDd9NLV2x3XwglTBbV6QA7B5KPyg3ke64KkGcekEbmcpJU5nz9xi-HyPGxFhLSfq3TFqpu77PWGu8PbyWdEY1iwRah7/s1600/IMG_20180428_145634.jpg" alt="Photo 5">
+    <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiejWXuWJOJY1o0vLngwpPgzsPkaKm0Oq4U1XVyuRS6HOaiYTBdM0F8Uck-qmOTIu_DUyFq5fspFreXlb9-gZKXaKuMpkqLlyeDh_T_7utHxOi8rkjdrFtt6cU4hdQmvY4wPimhxJ9P2WOp/s1600/IMG_20180607_113602.jpg" alt="Photo 6">
   </div>
    <div class="photo-grid">
-    <img src="/images/บอร์ดเกม6.jpg" alt="Photo 7">
-    <img src="/images/บอร์ดเกม7.jpg" alt="Photo 8">
-    <img src="/images/บอร์ดเกม8.jpg" alt="Photo 9">
-    <img src="/images/บอร์ดเกม9.jpg" alt="Photo 10">
-    <img src="/images/บอร์ดเกม10.jpeg" alt="Photo 11">
-    <img src="/images/บอร์ดเกม11.jpg" alt="Photo 12">
+    <img src="https://i.redd.it/ipd324n66cl61.jpg" alt="Photo 7">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Deskohran%C3%AD_08-09-27_212.jpg/640px-Deskohran%C3%AD_08-09-27_212.jpg" alt="Photo 8">
+    <img src="https://www.uplay.it/bggimages/14996/1146379.jpg" alt="Photo 9" >
+    <img src="https://www.uplay.it/bggimages/14996/1220301.jpg" alt="Photo 10">
+    <img src="https://www.uplay.it/bggimages/14996/1216204.jpg" alt="Photo 11">
+    <img src="https://www.uplay.it/bggimages/14996/1228301.jpg" alt="Photo 12">
   </div>
 </body>
 </html>
